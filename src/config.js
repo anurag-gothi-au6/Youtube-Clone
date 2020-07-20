bce5789ba0974f6cd95c6fa3a011557495e64f4d
anurag-gothi-au6 <advisoranurag@gmail.com>
2020-07-20 22:38:01 +0530

    Youtube with player and search functionality/

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 
 export default { 
-    CLIENT_ID: '817131102894-4cvrp9h5rpe1m0ksm9vjgt0k81afn4n4.apps.googleusercontent.com',
+    // CLIENT_ID: '817131102894-4cvrp9h5rpe1m0ksm9vjgt0k81afn4n4.apps.googleusercontent.com',
+    CLIENT_ID:'672737151355-b07tnobonujvfr51haveunm949km8oha.apps.googleusercontent.com',
     // API_KEY: 'AIzaSyDac6pg9aZNFiM8IfcPeOyXQ8Al_YNOCRQ',
     // API_KEY:'AIzaSyDL4yiQpbHgW4xat0kecYoPVSNAxT6mj1U',
     API_KEY:'AIzaSyDmRqiKZrE6zBiwKgfF3jDYp9szZm1AGOU',
