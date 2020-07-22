bf62626013aca46f41f7578f17340459d61e05c0
anurag-gothi-au6 <advisoranurag@gmail.com>
2020-07-22 23:13:54 +0530

    Ui improvement

--- src/pages/PlaylistPage.jsx
+++ src/pages/PlaylistPage.jsx
@@ -1,30 +1,45 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import {fetchPlaylist} from '../redux/actions/playlistActions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPlaylist } from "../redux/actions/playlistActions";
 import { Redirect } from "react-router-dom";
 
 class PlaylistPage extends Component {
-    componentDidMount(){
-        this.props.fetchPlaylist()
-    }
-    render() {
-        console.log(this.props.playlist)
-        return(
-        this.props.user===null?(<Redirect to='/login' />):
-        this.props.playlist!==null?(
-            <div>
-                {JSON.stringify(this.props.playlist)}
-            </div>
-        ):<div className="loader">loading...</div>
-        )
-    }
-}
- 
-const mapStateToProps = (storeState)=>{
-    return {
-        playlist: storeState.playlistState.playlist,
-        user: storeState.userState.user
-    }
+  componentDidMount() {
+    this.props.fetchPlaylist();
+  }
+  render() {
+    console.log(this.props.playlist);
+    return this.props.user === null ? (
+      <Redirect to="/login" />
+    ) : this.props.playlist !== null ? (
+      this.props.playlist.items.map((el) => (
+        <div class="card" key={el.id}>
+          <h5 class="card-header">Playlist</h5>
+          <div class="card-body">
+            <h5 class="card-title">{el.snippet.title}</h5>
+            <p class="card-text">
+            {el.snippet.description}
+            </p>
+            <button class="btn btn-primary">
+              Play Playlist
+            </button>
+          </div>
+        </div>
+      ))
+    ) : (
+      // <div>
+      //     {JSON.stringify(this.props.playlist)}
+      // </div>
+      <div className="loader">loading...</div>
+    );
+  }
 }
 
-export default connect(mapStateToProps,{fetchPlaylist})(PlaylistPage)
+const mapStateToProps = (storeState) => {
+  return {
+    playlist: storeState.playlistState.playlist,
+    user: storeState.userState.user,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPlaylist })(PlaylistPage);
